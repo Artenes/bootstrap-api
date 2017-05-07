75796676ce393fe680a4c331cc5bd08272ca85c9
Artenes Nogueira <artenesama@gmail.com>
2017-05-07 17:41:21 -0300

    Adds portuguese messages

---
+++ resources/lang/pt-br/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'no_credentials_found' => 'Você esqueceu de informar seu merchant-key e merchant-secret.',
+    'invalid_credentials' => 'As credenciais informadas são inválidas',
+    'error_while_processing_your_request' => 'Houve um erro ao processar sua requisição.',
+
+    'validation_error' => 'Erro de validação.',
+
+];
